bb88b49e87e6fae7373779556c74eec536668d10
Rafael Jesus <rafaelljesus86@gmail.com>
2016-11-06 18:48:38 -0200

    Expose proxy to users create action

--- handlers/routes.go
+++ handlers/routes.go
@@ -1,12 +1,29 @@
 package handlers
 
 import (
+	"encoding/json"
 	"github.com/labstack/echo"
+	"github.com/rafaeljesus/kyp-structs"
 	"net/http"
+	"os"
 )
 
+var KYP_USERS_ENDPOINT = os.Getenv("KYP_USERS_ENDPOINT")
+
+type User structs.User
+
 func UsersCreate(c echo.Context) error {
-	return c.JSON(http.StatusCreated, nil)
+	res, err := http.Post(KYP_USERS_ENDPOINT, "application/json; charset=utf-8", c.Request().Body())
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	user := User{}
+	json.NewDecoder(res.Body).Decode(&user)
+
+	return c.JSON(http.StatusCreated, user)
 }
 
 func UsersShow(c echo.Context) error {
