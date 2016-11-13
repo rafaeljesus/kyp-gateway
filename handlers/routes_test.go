6ecbc85182e48c5faab4a22f23e6e2caae98d89d
Rafael Jesus <rafaelljesus86@gmail.com>
2016-11-13 20:24:02 -0200

    Add tests for users create

---
+++ handlers/routes_test.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/h2non/gock.v1"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestUsersCreate(t *testing.T) {
+	defer gock.Off()
+	gock.New(KYP_USERS_ENDPOINT).
+		Post("/users").
+		Reply(201).
+		JSON(map[string]int{"id": 1})
+
+	response := `{"id":"1"}`
+	e := echo.New()
+	req, _ := http.NewRequest(echo.POST, "/api/v1/users", strings.NewReader(response))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	ctx := e.NewContext(standard.NewRequest(req, e.Logger()), standard.NewResponse(rec, e.Logger()))
+
+	if assert.NoError(t, UsersCreate(ctx)) {
+		assert.Equal(t, http.StatusCreated, rec.Code)
+	}
+}
